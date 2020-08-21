805bfa3e8b8e644830dff697d2e81f390bb046c8
alsrud9909 <csipms1023@gmail.com>
2020-08-21 15:23:05 +0900

    아이콘 수정

--- client/src/Chatbot/Sections/Message.js
+++ client/src/Chatbot/Sections/Message.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import Info from '../Sections/Info'
 import Card from '../Sections/Card'
-import { List, Icon, Avatar } from 'antd';
+import { List, Avatar } from 'antd';
 
 function Message(props) {
 
     var descrip
-    //const Image=<img src='C:/Users/kosta/Downloads/chatbot-app-master/chatbot-app-master/client/src/Chatbot/Sections/chatbotImage.png'/>
-    //width='10' height='10'
-    const AvatarSrc = props.who ==='구해줘 띵즈' ? <Icon type="robot" /> : <Icon type="smile" />  
+    const Image_bot=<img src='https://user-images.githubusercontent.com/59993071/90844507-0320ae00-e39f-11ea-9b58-1382a0949acd.png' alt='profile'/>
+    const Image_user=<img src='https://user-images.githubusercontent.com/59993071/90857536-4f7be600-e3bf-11ea-8ac8-5c0ddd226b26.png' alt='profile'/> 
 
         if(props.who ==='구해줘 띵즈'){
 
@@ -33,7 +32,6 @@ function Message(props) {
             else if(str.indexOf('things') !== -1){
 
                 str=Info(str)
-                // descrip=<div style={{backgroundColor: 'rgba(0,128,128,0.4)', padding : '5px 10px' , borderRadius: '0px 0px 5px 5px'}}><p>{str[0]}</p><p>{str[1]}</p><p>{str[2]}</p><p>{str[3]}</p></div>
                 descrip=<div style={{backgroundColor: 'rgba(0,128,128,0.4)', padding : '5px 5px 5px 5px ' , borderRadius: '0px 0px 5px 5px'}}>{str[0]}<br></br><div style={{backgroundColor: 'rgba(0,163,153,0.5)',padding : '5px 10px', borderRadius: '0px 0px 5px 5px'}}>{str[2]}<br></br>{str[3]}<br></br>{str[4]}</div></div>
             }
             else if(str.indexOf('회사정보') !== -1){
@@ -49,8 +47,7 @@ function Message(props) {
             return <div>
                 <List.Item style={{ padding: '0.5rem' }}>
                     <List.Item.Meta 
-                        avatar={<Avatar icon={AvatarSrc} />}
-                        ///avatar={Image}
+                        avatar={<Avatar icon={Image_bot} style={{margin: '8px 0px 0px 5px', width: '35px', height: '35px'}}/>}
                         title = {props.who}
                         description={descrip}
                     />
@@ -63,7 +60,7 @@ function Message(props) {
             return <div>
                 <List.Item style={{ padding: '0.5rem'}}>
                     <List.Item.Meta 
-                        avatar={<Avatar icon={AvatarSrc}/>}
+                        avatar={<Avatar icon={Image_user} style={{margin: '8px 0px 0px 5px', width: '35px', height: '35px'}}/>}
                         title = {props.who}
                         description={<div style={{backgroundColor: 'rgba(184,134,11,0.4)', padding : '5px 10px' , borderRadius: '0px 0px 5px 5px'}}> {props.text}</div>}
                     />
